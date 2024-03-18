0de474568a66d0d6a4843a26441bd80bf4856114
Dinesh <dinabeysinghe@gmail.com>
2024-03-18 14:34:28 +0530

    docchat css changed

--- app.py
+++ app.py
@@ -20,15 +20,14 @@ def get_pdf_text(pdf_docs):
     return text
 
 def get_excel_text(excel_docs):
-  text = ""
-  for excel_doc in excel_docs:
-      workbook = openpyxl.load_workbook(filename=excel_doc)
-      for sheet in workbook:
-          for row in sheet:
-              for cell in row:
-                  text += str(cell.value) + " "
-  return text.strip()
-
+    text = ""
+    for excel_doc in excel_docs:
+        workbook = openpyxl.load_workbook(filename=excel_doc)
+        for sheet in workbook:
+            for row in sheet:
+                for cell in row:
+                    text += str(cell.value) + " "
+    return text.strip()
 
 def get_text_chunks(text):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20)
@@ -55,53 +54,51 @@ def get_user_input(user_question):
         with left:
             for i, message in enumerate(st.session_state.chatHistory):
                 if i % 2 == 0:
-                    st.write("User: ", message.content)
+                    st.markdown(f'<div style="background-color: rgb(30 24 17 / 77%); border-radius: 10px; padding: 10px; margin-bottom: 5px; text-align: end;"><span style="text-align: end;">User:</span> {message.content}</div>', unsafe_allow_html=True)
                 else:
-                    st.write("Bot: ", message.content)
-            st.success("Done !")
+                    st.markdown(f'<div style="background-color: rgb(145 74 1 / 25%); border-radius: 10px; padding: 10px; margin-bottom: 5px; ">Bot: {message.content}</div>', unsafe_allow_html=True)
         with right:
             for message in st.session_state.chatHistory:
                 file_contents += f"{message.content}\n"
             file_name = "Chat_History.txt"
 
 def main():
-  st.set_page_config("DocChat")
-  st.header("DocChat - Chat with multiple documents")
-  st.write("---")
-  with st.container():
-    with st.sidebar:
-      st.title("Settings")
-      st.subheader("Upload Documents")
-      st.markdown("**PDF files:**")
-      pdf_docs = st.file_uploader("Upload PDF Files", accept_multiple_files=True)
-      if st.button("Process PDF file"):
-        with st.spinner("Processing PDFs..."):
-          raw_text = get_pdf_text(pdf_docs)
-          text_chunks = get_text_chunks(raw_text)
-          vector_store = get_vector_store(text_chunks)
-          st.session_state.conversation = get_conversational_chain(vector_store)
-          st.success("PDF processed successfully!")
+    st.set_page_config("DocChat")
+    st.header("DocChat - Chat with multiple documents")
+    st.write("---")
+    with st.container():
+        with st.sidebar:
+            st.title("Settings")
+            st.subheader("Upload Documents")
+            st.markdown("**PDF files:**")
+            pdf_docs = st.file_uploader("Upload PDF Files", accept_multiple_files=True)
+            if st.button("Process PDF file"):
+                with st.spinner("Processing PDFs..."):
+                    raw_text = get_pdf_text(pdf_docs)
+                    text_chunks = get_text_chunks(raw_text)
+                    vector_store = get_vector_store(text_chunks)
+                    st.session_state.conversation = get_conversational_chain(vector_store)
+                    st.success("PDF processed successfully!")
 
-      st.markdown("**Excel files:**")
-      excel_docs = st.file_uploader("Upload Excel Files", accept_multiple_files=True)
-      if st.button("Process Excel file"):
-        with st.spinner("Processing Excel files..."):
-          raw_text = get_excel_text(excel_docs)
-          text_chunks = get_text_chunks(raw_text)
-          vector_store = get_vector_store(text_chunks)
-          st.session_state.conversation = get_conversational_chain(vector_store)
-          st.success("Excel file processed successfully!")
+            st.markdown("**Excel files:**")
+            excel_docs = st.file_uploader("Upload Excel Files", accept_multiple_files=True)
+            if st.button("Process Excel file"):
+                with st.spinner("Processing Excel files..."):
+                    raw_text = get_excel_text(excel_docs)
+                    text_chunks = get_text_chunks(raw_text)
+                    vector_store = get_vector_store(text_chunks)
+                    st.session_state.conversation = get_conversational_chain(vector_store)
+                    st.success("Excel file processed successfully!")
 
-  with st.container():
-      st.subheader("Document Q&A")
-      st.write('Ask a question : ')
-      user_question = st.text_input("Ask a Question from the document")
-      if "conversation" not in st.session_state:
-          st.session_state.conversation = None
-      if "chatHistory" not in st.session_state:
-          st.session_state.chatHistory = None
-      if user_question:
-          get_user_input(user_question)
+    with st.container():
+        st.subheader("Document Q&A")
+        user_question = st.text_input("Ask a Question from the document")
+        if "conversation" not in st.session_state:
+            st.session_state.conversation = None
+        if "chatHistory" not in st.session_state:
+            st.session_state.chatHistory = None
+        if user_question:
+            get_user_input(user_question)
 
 if __name__ == "__main__":
     main()
